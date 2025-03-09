8e6656083d7dfe15bda3dc6e8828a775d78c009c
ANGEL PEDREROS <angel.pedreros-a@mail.escuelaing.edu.co>
2025-03-08 22:45:35 -0500

    Añadiendo problemas pedidos en clase - polinomios y caracteres completos

---
+++ polinomios/build/CMakeFiles/polinomios.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/polinomios.dir/polinomios.cpp.o"
+  "CMakeFiles/polinomios.dir/polinomios.cpp.o.d"
+  "polinomios"
+  "polinomios.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/polinomios.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
